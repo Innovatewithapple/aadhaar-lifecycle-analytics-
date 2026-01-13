d98a3a2025be36a7fd86e391a7af695b5e0c50f7
Mihir Vyas <mihirv126@gmail.com>
2026-01-13 18:40:26 +0530

    stable

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 class Person:
+    count = 0
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        Person.getcountofPerson()
+
+    @classmethod
+    def getcountofPerson(cls):
+        cls.count += 1
